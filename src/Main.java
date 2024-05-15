bc287a1d4a1ac9eb042b16fa5136b039a2e2e124
Tafitasoa Ranoarivony <72605753+TafitAn@users.noreply.github.com>
2024-05-15 21:39:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,26 @@ public class Main {
         ArrayUtil arrayUtil = new ArrayUtil(7);
         arrayUtil.arrayDemo();
         arrayUtil.removeEven();
+        
         ReverseArray reverseArray = new ReverseArray();
         int[] myArray = {1, 2, 3, 4, 5, 6, 7};
         reverseArray.reverse(myArray, 0, 6);
+        
         FindMinimumValue f = new FindMinimumValue();
         int[] myArray1 = {45, 122, 69, 78, 266, 9874, 58, 23, 45 ,16, 1548};
         System.out.println("La valeur minimum du tableau est: " + f.findMinVal(myArray1));
         Find2ndMaxValue find2ndMaxValue = new Find2ndMaxValue();
         System.out.println("La valeur de la 2nde maximale est: " + find2ndMaxValue.find2ndMaxVal(myArray1));
         System.out.println("La valeur de la 2nde maximale est: " + find2ndMaxValue.find2ndMaxVal2(myArray1));
+        
         Move0AtEnd move0AtEnd = new Move0AtEnd();
         int[] myArray3 = {45, 0, 122, 0, 69, 0, 78, 266, 9874, 58, 23, 45 ,16, 1548};
         System.out.println("Basculer 0 vers la dernière index: " );
         move0AtEnd.moveZeroEnd(myArray3);
         move0AtEnd.moveZeroEnd2(myArray3);
+
+        MoveStrAtEnd moveStrAtEnd = new MoveStrAtEnd();
+        String[] arrayStr = {"A", "B", "C", "D", "B", "E", "F", "G", "B", "H", "I"};
+        move0AtEnd.moveBAtEnd(arrayStr);
     }
 }
