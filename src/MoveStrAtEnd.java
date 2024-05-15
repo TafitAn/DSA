d074a9b14a5cc2d5d98056546ac42bb8fb564266
Tafitasoa Ranoarivony <72605753+TafitAn@users.noreply.github.com>
2024-05-15 21:38:34 +0300

    Create MoveStrAtEnd.java

---
+++ src/MoveStrAtEnd.java
@@ -0,0 +1,23 @@
+public class MoveStrAtEnd {
+    
+    public MoveStrAtEnd() {
+    }
+    
+    public void moveBAtEnd(String[] arr){
+        int n = arr.length;
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if( arr[i] != "B" && arr[j] == "B"){
+                String temp = arr[i];
+                arr[i] = arr [j];
+                arr[j] = temp;
+            }
+            if(arr[j] != "B"){
+                j++;
+            }
+            for(String k : arr){
+                System.out.print(k + ' ');
+            }
+            System.out.print("\n");
+        }
+    }
