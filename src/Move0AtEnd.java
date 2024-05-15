7cf6bc24a6bfe64855c509850033b45aeabda796
Ranoarivony <andrintsu@gmail.com>
2024-05-15 10:55:23 +0300

    Update Move zero at end

--- src/Move0AtEnd.java
+++ src/Move0AtEnd.java
@@ -3,15 +3,23 @@ public class Move0AtEnd {
     }
     public void moveZeroEnd(int[] arr){
         int temp;
-        for (int i = 0; i < (arr.length - 1); i++){
+        int zeroCount = 0;
+        for (int i = 0; i < arr.length; i++){
             if (arr[i] == 0){
-                temp = arr[i+1];
-                arr[i+1] = 0;
-                arr[i] = temp;
-                for (int k : arr){
-                    System.out.print(k +" ");
+                zeroCount++;
+            }
+        }
+        for(int j = 0; j < zeroCount; j++) {
+            for (int i = 0; i < (arr.length - 1); i++) {
+                if (arr[i] == 0) {
+                    temp = arr[i + 1];
+                    arr[i + 1] = 0;
+                    arr[i] = temp;
+                    for (int k : arr) {
+                        System.out.print(k + " ");
+                    }
+                    System.out.println("");
                 }
-                System.out.println("");
             }
         }
     }
