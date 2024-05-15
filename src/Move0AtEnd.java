cc65b85c63eefbc855ce89b5237043834e0a58de
Tafitasoa Ranoarivony <72605753+TafitAn@users.noreply.github.com>
2024-05-15 18:23:27 +0300

    Update Move0AtEnd.java

--- src/Move0AtEnd.java
+++ src/Move0AtEnd.java
@@ -23,4 +23,23 @@ public class Move0AtEnd {
             }
         }
     }
+
+    public void moveZeroEnd2(int[] arr){
+        int j = 0;
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            if (arr[j] != 0){
+                j++;
+            }
+            for(int k : arr){
+                System.out.print(k + " ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
